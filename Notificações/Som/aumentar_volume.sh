72be94a3984fe390a466b0a392d7c32a9d43c308
Leonardo <leonardo.siriaco@gmail.com>
2022-07-09 14:55:46 -0300

    Alterado nome do diretório

---
+++ Notificações/Som/aumentar_volume.sh
@@ -0,0 +1,9 @@
+#	          o     |                   o|        |     
+#	,---.,---..,---.|--- ,---.,---.,---..|    ,---|,---.
+#	|   |,---||`---.|    |    |   ||   |||    |   ||   |
+#	|---'`---^``---'`---'`    `---'`---|``---'`---'`---'
+#	|                              `---'                
+
+# Aumentando volume em 5% e notificando
+amixer set Master 5%+ 
+~/Documentos/Notificações/Som/mostrar_volume.sh
